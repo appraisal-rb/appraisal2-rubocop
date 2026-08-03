5964a37bc8517fd1b68953ae133563b3696725ad
Peter H. Boling <peter.boling@gmail.com>
2026-08-03 17:35:17 -0600

    🎨 Template bootstrap by kettle-jem v7.1.0

--- spec/appraisal2/rubocop/version_spec.rb
+++ spec/appraisal2/rubocop/version_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "anonymous_loader"
+require "appraisal2/rubocop"
 RSpec.describe Appraisal2::Rubocop::Version do
   it_behaves_like "a Version module", described_class
 
